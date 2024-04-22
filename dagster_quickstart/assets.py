9d3a1489d8d8be0b753c8936eba69d87db8ea0ef
Sami <hellosami@protonmail.com>
2024-04-22 15:37:12 +0200

    Fetch csv data from URL and create asset

--- dagster_quickstart/assets.py
+++ dagster_quickstart/assets.py
@@ -1,10 +1,31 @@
 import json
 import requests
-
-import pandas as pd
+import polars
+import io
 
 from dagster import (
     MaterializeResult,
     MetadataValue,
     asset,
 )
+
+
+    
+@asset
+def people_100_asset():
+    url = "https://drive.google.com/uc?id=1phaHg9objxK2MwaZmSUZAKQ8kVqlgng4&export=download"
+    filename = "people-100"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        
+        dataframe = polars.read_csv(io.StringIO(response.text))
+        
+        dataframe.write_csv(f"{filename}.csv")
+
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    
+    return dataframe
