bd1b3138efaae7e44a4c19a2ca6f531b4d5c81d9
Sami <hellosami@protonmail.com>
2024-04-24 11:20:41 +0200

    Refactor: add sort, reorder functions, clean up

--- dagster_quickstart/assets.py
+++ dagster_quickstart/assets.py
@@ -1,9 +1,7 @@
-import json
 import requests
 import io
 import polars
 import datetime
-import pandas as pd
 
 from dagster import (
     MaterializeResult,
@@ -11,12 +9,11 @@ from dagster import (
     asset,
 )
 
-    
+url = "https://drive.google.com/uc?id=1phaHg9objxK2MwaZmSUZAKQ8kVqlgng4&export=download"
+filename = "people-100"
+
 @asset
 def people_asset():
-    url = "https://drive.google.com/uc?id=1phaHg9objxK2MwaZmSUZAKQ8kVqlgng4&export=download"
-    filename = "people-100"
-
     try:
         response = requests.get(url)
         response.raise_for_status()  
@@ -31,42 +28,6 @@ def people_asset():
     
     return dataframe
 
-
-@asset(deps=[people_asset])
-def unique_firstnames() -> MaterializeResult:
-    dataframe = people_asset()
-    column = "First Name"
-
-    unique_firstnames = dataframe.select(column).unique()
-
-    preview = str(unique_firstnames)
-
-    return MaterializeResult(
-        metadata={
-            "num_unique_firstnames": len(unique_firstnames),
-            "preview": MetadataValue.md(preview),
-        }
-    )
-    
-    
-@asset(deps=[people_asset])
-def anonymization_by_lastname() -> MaterializeResult:
-    dataframe = people_asset()
-    column = "Last Name"
-
-    shuffled_lastnames = polars.col(column).shuffle(seed=1)
-    
-    dataframe_shuffled = dataframe.with_columns(shuffled_lastnames.alias(column))
-
-    preview = str(dataframe_shuffled)
-
-    return MaterializeResult(
-        metadata={
-            "num_shuffled_lastnames": len(dataframe_shuffled),
-            "preview": MetadataValue.md(preview),
-        }
-    )
-    
 
 def helper_calculate_age(birthdates: polars.Series) -> polars.Series:
     today = datetime.date.today()
@@ -97,34 +58,10 @@ def people_and_age_asset():
     dataframe = dataframe.with_columns(ages.alias(column_age))
     
     return dataframe
-    
-    
-@asset(deps=[people_and_age_asset])
-def mean_age_per_profession() -> MaterializeResult:
-    dataframe = people_and_age_asset()
-    column_profession = "Job Title"
-    column_age = "Age"
-    
-    # Group by job title and find mean age
-    mean_age_group_by_profession = (
-        dataframe.groupby(column_profession)
-        .agg(polars.mean(column_age).alias("Mean Age"))
-    )
-        
-    print(mean_age_group_by_profession)
-    
-    preview = str(mean_age_group_by_profession)
-    
-    return MaterializeResult(
-        metadata={
-            "num_mean_age": len(mean_age_group_by_profession),
-            "preview": MetadataValue.md(preview),
-        }
-    )
 
-    
+
 @asset(deps=[people_and_age_asset])
-def funny_ages():
+def funny_ages_asset():
     dataframe = people_and_age_asset()
     column_profession = "Job Title"
     column_birthdate = "Date of birth"
@@ -135,11 +72,80 @@ def funny_ages():
     funny_ages = (
         dataframe.select([column_firstname, column_profession, column_birthdate, column_age])
         .filter((dataframe[column_age] < 13))
+        .sort(column_age)
     )
     
-    print(funny_ages)
+    return funny_ages
+
+
+@asset(deps=[people_asset])
+def unique_firstnames() -> MaterializeResult:
+    dataframe = people_asset()
+    column = "First Name"
+
+    unique_firstnames = dataframe.select(column).unique()
+
+    preview = str(unique_firstnames)
+
+    return MaterializeResult(
+        metadata={
+            "num_unique_firstnames": len(unique_firstnames),
+            "preview": MetadataValue.md(preview),
+        }
+    )
+
+
+@asset(deps=[people_asset])
+def anonymization_by_lastname() -> MaterializeResult:
+    dataframe = people_asset()
+    column = "Last Name"
+
+    shuffled_lastnames = polars.col(column).shuffle(seed=1)
     
+    dataframe_shuffled = dataframe.with_columns(shuffled_lastnames.alias(column))
+
+    preview = str(dataframe_shuffled)
+
+    return MaterializeResult(
+        metadata={
+            "num_shuffled_lastnames": len(dataframe_shuffled),
+            "preview": MetadataValue.md(preview),
+        }
+    )
+
+
+@asset(deps=[people_and_age_asset])
+def mean_age_per_profession() -> MaterializeResult:
+    dataframe = people_and_age_asset()
+    column_profession = "Job Title"
+    column_age = "Age"
     
-mean_age_per_profession()
-funny_ages()
-        
+    # Group by job title and find mean age
+    mean_age_group_by_profession = (
+        dataframe.groupby(column_profession)
+        .agg(polars.mean(column_age).alias("Mean Age"))
+        .sort(column_profession)
+    )
+            
+    preview = str(mean_age_group_by_profession)
+    
+    return MaterializeResult(
+        metadata={
+            "num_mean_age": len(mean_age_group_by_profession),
+            "preview": MetadataValue.md(preview),
+        }
+    )
+
+
+@asset(deps=[funny_ages_asset])
+def funny_ages():
+    dataframe = funny_ages_asset()
+    
+    preview = str(dataframe)
+    
+    return MaterializeResult(
+        metadata={
+            "num_funny_ages": len(dataframe),
+            "preview": MetadataValue.md(preview),
+        }
+    )
